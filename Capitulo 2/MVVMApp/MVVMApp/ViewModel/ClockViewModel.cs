23ddacb97f24c422316d43b2a3e982c35894e535
flinares <flinaresmata@gmail.com>
2024-10-23 14:48:44 +0000

    mis cambuos

---
+++ Capitulo 2/MVVMApp/MVVMApp/ViewModel/ClockViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MVVMApp.ViewModel
+{
+    public class ClockViewModel
+    {
+        public System.Timers.Timer timer { get; set; }
+        public string CurrentTime { get; set; }
+        public string Name { get; set; }
+        public ClockViewModel() { 
+            CurrentTime = DateTime.Now.ToString("hh:mm:ss");
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Update_Time;
+            timer.Start();
+        }
+
+        public void Update_Time(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            this.CurrentTime = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        public void Dispose()
+        {
+            timer.Dispose();
+        }
+    }
+}
